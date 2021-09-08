5338dc68cd41aa2632dfe6454bc7d3427331159f
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2021-09-08 19:07:50 +0200

    add HelloWord class

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,6 @@
+public class FirstClass {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!");
+    }
+
+}
---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    static String welcome = "Hello World!";
+    static String name = "Piotr";
+    static char surname = 'N';
+    static int age = 19;
+
+    public static void main (String[] args){
+        System.out.println(welcome);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(surname);
+
+    }
+}
