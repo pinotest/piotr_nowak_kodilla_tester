3a6068f03959bfa51405c587997f40305afc47f6
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2021-09-08 19:19:36 +0200

    add MainExtended class

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String myWelcomeSentence = "Hello!";
+        char myFirstSurnameLetter = 'N';
+        int myFavouriteAge = 29;
+        double mySalary = 10.12;
+        boolean isThatCorrect = true;
+        byte exampleByte = 0;
+        System.out.println(myWelcomeSentence);
+        System.out.println(myFirstSurnameLetter);
+        System.out.println(myFavouriteAge);
+        System.out.println(mySalary);
+        System.out.println(isThatCorrect);
+        System.out.println(exampleByte);
+    }
+}
