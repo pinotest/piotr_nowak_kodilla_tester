36d5ca2777ecec3aab1897f79f810e426d9ff656
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2021-10-18 15:17:54 +0200

    Change Book class return type to Book..

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,10 +2,12 @@ public class Book {
     private String author;
     private String title;
 
-    public static Object of(String author, String title){
+    public static Book of(String author, String title){
+
         Book object = new Book();
         object.author = author;
         object.title = title;
         return object;
+
     }
 }
