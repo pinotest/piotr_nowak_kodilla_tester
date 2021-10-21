0851a6da1680d87e37c85140131ae298b5f7683d
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2021-10-21 14:47:26 +0200

    Add new package basic_assertion

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,31 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int subtract = calculator.subtract(a,b);
+        boolean correctSub = ResultChecker.assertEquals(-3, subtract);
+        if (correctSub) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int squere = calculator.squere(a);
+        boolean correctSquere = ResultChecker.assertEquals(25, squere);
+        if (correctSub) {
+            System.out.println("Metoda squere działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda squere nie działa poprawnie dla liczb " + a );
+        }
+
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    public int sum(int firstNumber, int secondNumber){
+        return firstNumber+secondNumber;
+    }
+    public int subtract(int firstNumber, int secondNumber){
+        return firstNumber-secondNumber;
+    }
+    public int squere(int number){
+        return number*number;
+    }
+}
