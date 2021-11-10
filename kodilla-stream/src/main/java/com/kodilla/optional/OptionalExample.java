be222e3e5404f48fdc0eb44fc4f6c437780e1fcc
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2021-11-10 16:18:00 +0100

    Add optional package

---
+++ kodilla-stream/src/main/java/com/kodilla/optional/OptionalExample.java
@@ -0,0 +1,16 @@
+package com.kodilla.optional;
+
+import com.kodilla.stream.User;
+
+import java.util.Optional;
+
+public class OptionalExample {
+    public static void main(String[] args) {
+        User user = new User("user1", 30, 100, "Test");
+
+        Optional<User> optionalUser = Optional.ofNullable(user);
+        String username =
+                optionalUser.orElse(new User("", 0, 0, "")).getUsername();     // [1]
+
+        optionalUser.ifPresent(u -> System.out.println(u.getUsername()));    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -0,0 +1,26 @@
+package com.kodilla.optional.homework;
+
+import com.kodilla.stream.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add( new Student("Jacob Novac", new Teacher("Stephen King")));
+        students.add( new Student("Ben Kowalski", new Teacher("Stephen King")));
+        students.add( new Student("Tommy Jacobsen", null));
+        students.add( new Student("Adel Ada", null));
+        students.add( new Student("Max King", new Teacher("Stephen Queen")));
+
+        for (Student student: students
+             ) {
+
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(student.teacher);
+            String teacher = optionalTeacher.orElse(new Teacher("<undefined>")).getName();
+            System.out.println("uczeń: " + student.getName() + " nauczyciel: " + teacher);
+        }
+    }
+}
