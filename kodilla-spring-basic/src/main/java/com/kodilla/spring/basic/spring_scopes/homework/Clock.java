a3c6f209348878d234604f1f3b5e524945331f51
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2021-12-06 18:12:24 +0100

    Add int random generation to Clock.class

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
+import java.util.Random;
 
 @Component
 @Scope("prototype")
 public class Clock {
     public Clock(){
         LocalTime localTime = LocalTime.now();
+        Random random = new Random();
+        System.out.println("Random Number: "+random.nextInt(100));
         System.out.println("Actual time: "+ localTime);
     }
 }
