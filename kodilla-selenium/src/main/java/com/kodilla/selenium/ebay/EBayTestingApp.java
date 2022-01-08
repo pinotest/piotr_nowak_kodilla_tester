20c883851607d10865e512aa0a361b6303803e23
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2022-01-08 11:12:35 +0100

    Add new selenium module

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -1,2 +1,23 @@
-package com.kodilla.selenium.ebay;public class EBayTestingApp {
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class EBayTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com");
+        driver.manage().window().maximize();
+
+        WebElement inputField = driver.findElement(By.name("_nkw")); // [4]
+        inputField.sendKeys("laptop");
+        inputField.submit();
+
+
+        driver.close();
+    }
 }
--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -1,2 +1,20 @@
-package com.kodilla.selenium.google;public class GoogleExample {
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        WebElement inputField = driver.findElement(By.name("q")); // [4]
+        inputField.sendKeys("Kodilla");
+        inputField.submit();
+
+    }
 }
